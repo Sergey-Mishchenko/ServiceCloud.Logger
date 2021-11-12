75c2e0528e5999b39be62e56511b7932cae117c3
Deryagin.Alex <Deryagin.Alex@outlook.com>
2021-11-12 08:24:53 +0300

    version 3

--- ServiceCloud.Logger.Test/ITextFileLoggerTests.cs
+++ ServiceCloud.Logger.Test/ITextFileLoggerTests.cs
@@ -1,52 +1,52 @@
 using NUnit.Framework;
 using ServiceCloud.Logger;
+using System.Text;
+using System;
+using System.Collections.Generic;
 
 namespace ServiceCloud.Logger.Test {
     [TestFixture]
-    public class ITextFileLoggerTests {
+    abstract public class ITextFileLoggerTests {
+        protected abstract ITextFileLogger FactoryMethod();
 
-        abstract class CreatorTestObject {   
-            public abstract TestObject FactoryMethod();
-        }
+        [Test]
+        public void ITextFileLogger_Encoding_DefaultValue_Test() {
+            ITextFileLogger tfLogger = FactoryMethod();
+            string target = tfLogger.ToString();
 
-        class CreatorTestObjectA : CreatorTestObject {
-            public CreatorTestObjectA() { }
-
-            public override TestObject FactoryMethod() {
-                return new TestObjectA();
-            }
-        }
-
-        abstract class TestObject {
-            public abstract string FilePath { get; }
-        }
-
-        class TestObjectA : TestObject {
-            private readonly string path;
-
-            public TestObjectA() {
-                path = @"D:\Source\Patterns & Delegate & OOP\Factory Method";
-
-            }
-
-            public override string FilePath {
-                get { return path; }
-            }
+            Assert.IsFalse(string.IsNullOrEmpty(target));
         }
 
         [Test]
-        public void ITextFileLogger_FilePath_checkDefaultValue_Test() {
+        public void ITextFileLogger_FilePath_DefaultValue_Test() {
+            ITextFileLogger target = FactoryMethod();
 
-            CreatorTestObject c = new CreatorTestObjectA();
-            TestObject some_object = new TestObjectA();
-            string result = some_object.FilePath;
-
-           
-            Assert.IsNotNull(result);
-            Assert.AreNotEqual("", result);
+            Assert.IsFalse(string.IsNullOrEmpty(target.FilePath));
         }
 
+        [Test]
+        public void ITextFileLogger_MessageDelimiter_DefaultValue_Test() {
+            ITextFileLogger target = FactoryMethod();
 
+            Assert.IsFalse(string.IsNullOrEmpty(target.MessageDelimiter));
+        }
+
+        [Test]
+        public void ITextFileLogger_MessageFormatter_DefaultValue_Test() {
+            ITextFileLogger ftLogger = FactoryMethod();
+            IMessageFormatter mFormatter = ftLogger.MessageFormatter;
+            string target = mFormatter.ToString();
+
+            Assert.IsFalse(string.IsNullOrEmpty(target));
+        }
+
+        [Test]
+        public void ITextFileLogger_MessageFormatter_DefaultEntry_Test() {
+            ITextFileLogger target = FactoryMethod();
+           
+            
+
+        }
 
 
 
